7bbe74b367c01f09c0ada9d4cbbb2579926fa98d
Naiztu <A01707404@itesm.mx>
2021-05-19 20:23:48 -0500

    Implementación de clase cajero

--- main.cpp
+++ main.cpp
@@ -6,17 +6,18 @@ By: José Ángel Rico Mendieta - A01707404
 #include "Personal.h"
 #include "Cliente.h"
 #include "Proveedor.h"
+#include "Cajero.h"
 
 int main() {
     
     //Objeto de la clase padre para su presentación
  
     cout << "\nCLASE PADRE:\n";
-    Personal pablo_sinDatos = Personal(1,"Pablo de La Purísima Concepción", 123456789);
-    pablo_sinDatos.printPersonal();
+    Personal per_sinDatos = Personal(1,"Pablo de La Purísima Concepción", 123456789);
+    per_sinDatos.printPersonal();
 
-    Personal pablo_conDatos = Personal(1,"Pablo de La Purísima Concepción","pablito@gmail.com","Av. México #204", 123456789);
-    pablo_conDatos.printPersonal();
+    Personal per_conDatos = Personal(1,"Pablo de La Purísima Concepción","pablito@gmail.com","Av. México #204", 123456789);
+    per_conDatos.printPersonal();
 
 
     //Objeto de clase cliente para su presentación
@@ -37,6 +38,17 @@ int main() {
 
     Proveedor prov_conDatos = Proveedor(1,"Raul de La Purísima Concepción","raulito@gmail.com","Av. Tlaxcala #204", 987654321, "Cuplasa");
     prov_conDatos.printPersonal();
+
+    //Objeto de clase cajero para su presentación
+
+    cout << "\n\nCLASE CAJERO:\n";
+    Cajero caj_sinDatos = Cajero(1,"Fermin de La Purísima Concepción", 918273640, 18);
+    caj_sinDatos.printPersonal();
+
+    Cajero caj_conDatos = Cajero(1,"Fermin de La Purísima Concepción","fermincito@gmail.com","Av. Roma #204", 918273640, 18);
+    caj_conDatos.printPersonal();
+
+    caj_conDatos.calcularSalario(48);
 
     return 0;
 }
