4a4ccebacf4a178d599ec62c9a12fdc63731c2a8
Naiztu <A01707404@itesm.mx>
2021-05-19 19:27:08 -0500

    Implementación de clase cliente

--- main.cpp
+++ main.cpp
@@ -3,18 +3,29 @@ Programación orientada a objetos - TC1030.1
 By: José Ángel Rico Mendieta - A01707404
 **/
 
-#include "Personal.h"
+//#include "Personal.h"
+#include "Cliente.h"
 
 int main() {
     
-    //Creamos un objeto de la clase padre para su presentación
+    //Objeto de la clase padre para su presentación
+ 
+    cout << "\nCLASE PADRE:\n";
     Personal pablo_sinDatos = Personal(1,"Pablo de La Purísima Concepción", 123456789);
     pablo_sinDatos.printPersonal();
 
-    cout << "\n\n";
-
     Personal pablo_conDatos = Personal(1,"Pablo de La Purísima Concepción","pablito@gmail.com","Av. México #204", 123456789);
     pablo_conDatos.printPersonal();
+
+    cout << "\n";
+
+    cout << "\nCLASE CLIENTE:\n";
+    //Objeto de clase cliente
+    Cliente cliente_sinDatos = Cliente(1,"Juan de La Purísima Concepción", 567891234, "JDPC20342");
+    cliente_sinDatos.printPersonal();
+
+    Cliente cliente_conDatos = Cliente(1,"Juan de La Purísima Concepción","juanito@gmail.com","Av. Japón #204", 567891234, "JDPC20342");
+    cliente_conDatos.printPersonal();
 
     return 0;
 }
