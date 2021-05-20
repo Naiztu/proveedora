9efd3ffc094dd8cc83fda0a40c160ab05b54e891
Naiztu <A01707404@itesm.mx>
2021-05-19 19:59:32 -0500

    Implementación de clase proveedor

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@ Programación orientada a objetos - TC1030.1
 By: José Ángel Rico Mendieta - A01707404
 **/
 
-//#include "Personal.h"
+#include "Personal.h"
 #include "Cliente.h"
+#include "Proveedor.h"
 
 int main() {
     
@@ -17,15 +18,25 @@ int main() {
     Personal pablo_conDatos = Personal(1,"Pablo de La Purísima Concepción","pablito@gmail.com","Av. México #204", 123456789);
     pablo_conDatos.printPersonal();
 
-    cout << "\n";
 
-    cout << "\nCLASE CLIENTE:\n";
-    //Objeto de clase cliente
+    //Objeto de clase cliente para su presentación
+
+    cout << "\n\nCLASE CLIENTE:\n";
     Cliente cliente_sinDatos = Cliente(1,"Juan de La Purísima Concepción", 567891234, "JDPC20342");
     cliente_sinDatos.printPersonal();
 
     Cliente cliente_conDatos = Cliente(1,"Juan de La Purísima Concepción","juanito@gmail.com","Av. Japón #204", 567891234, "JDPC20342");
     cliente_conDatos.printPersonal();
+
+
+    //Objeto de clase proveedor para su presentación
+
+    cout << "\n\nCLASE PROVEEDOR:\n";
+    Proveedor prov_sinDatos = Proveedor(1,"Raul de La Purísima Concepción", 987654321, "Cuplasa");
+    prov_sinDatos.printPersonal();
+
+    Proveedor prov_conDatos = Proveedor(1,"Raul de La Purísima Concepción","raulito@gmail.com","Av. Tlaxcala #204", 987654321, "Cuplasa");
+    prov_conDatos.printPersonal();
 
     return 0;
 }
